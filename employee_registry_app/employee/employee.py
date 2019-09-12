a519cb555e62976311eddf7e9a312b6625efc2ec
Hassan Elbaytam <hassan.elbaytam@gmail.com>
2019-09-11 20:48:03 -0400

    gitignore

--- employee_registry_app/employee/employee.py
+++ employee_registry_app/employee/employee.py
@@ -4,15 +4,15 @@ class Employee:
 
     def __init__(self, first, last, pay):
         self.id = Employee.employee_count + 1
-        self.first = first
-        self.last = last
+        self.first = first.lower()
+        self.last = last.lower()
         self.email = first.lower() + '.' + last.lower() + '@company.com'
         self.pay = pay
 
         Employee.employee_count += 1
 
     def fullname(self):
-        return '{} {}'.format(self.first, self.last)
+        return '{} {}'.format(self.first.capitalize(), self.last.capitalize())
 
     def apply_raise(self):
         self.pay = round(int(self.pay) * Employee.raise_amount, 2)
